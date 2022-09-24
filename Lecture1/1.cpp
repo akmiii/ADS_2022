1e4bb9a2481938716ade696963b6cf1c14e48d98
AKMIRA SAGATBEK <akmira2410@mail.ru>
2022-09-24 10:33:20 +0600

    Lecture 1

---
+++ Lecture1/1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    /*
+        Big O notation - O(n):
+    parameters: time and memory
+    n - the number of iterations
+    linear addiction: y = kx, where k should be small
+    if n = 10^8 => 1sec
+
+    O(c) - const addiction
+    O(log(n)) - logarithmic addiction
+
+    O(1) < O(log(n)) < O(n) < O(n^2)
+    Positive disruption - optimal solution
+    
+        Data structure:
+    A data structure is a specialized format for organizing, processing, retrieving and storing data.
+
+                              Доступ      Поиск       Вставка      Удаление
+------------------------------------------------------------------------
+                Массив    O(1)        O(N)        O(N)         O(N)
+        Связный список    O(N)        O(N)        O(1)         O(1)
+Двоичное дерево поиска    O(log N)    O(log N)    O(log N)     O(log N)
+
+
+    reading chapters of the book: 
+    - introduction
+    - complexity and efficience (5th chapter)
+    
+    */
+
+}
