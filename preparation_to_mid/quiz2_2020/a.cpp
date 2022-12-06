899a857645cc1134b3df22cdb272774f660fc2c7
AKMIRA SAGATBEK <akmira2410@mail.ru>
2022-12-07 00:12:50 +0600

    quiz2_2020

---
+++ preparation_to_mid/quiz2_2020/a.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+struct MaxHeap{
+    vector<int> heap;
+
+    int parent(int i){
+        return (i - 1)/2;
+    }
+    int left(int i){
+        return 2 * i + 1;
+    }
+    int right(int i){
+        return 2 * i + 2;
+    }
+
+    void insert(int val){
+        heap.push_back(val);
+        int i = heap.size() - 1;
+        while(i != 0 && heap[parent(i)] < heap[i]){
+            swap(heap[i], heap[parent(i)]);
+            i = parent(i);
+        }
+    }
+
+    int extractMax(){
+        if (heap.size() == 0) return 0;
+        if (heap.size() == 1){
+            int root = heap[0];
+            heap.pop_back();
+            return root;
+        }
+        int root = heap[0];
+        heap[0] = heap[heap.size() - 1];
+        heap.pop_back();
+        heapify(0);
+        return root;
+    }
+
+    void heapify(int i){
+        int l = left(i), r = right(r);
+        int smallest = i;
+        if (l < heap.size() && heap[l] > heap[i]) smallest = l;
+        if (r < heap.size() && heap[r] >  heap[smallest]) smallest = r;
+        if (smallest != i){
+            swap(heap[smallest], heap[i]);
+            heapify(smallest);
+        }
+    }
+};
+
+int main(){
+    int n, k;
+    MaxHeap *maxHeap = new MaxHeap();
+    cin >> n >> k;
+    int sum = 0;
+    while(n--){
+        int x;
+        cin >> x;
+        maxHeap -> insert(x);
+    }
+    while(k--){
+        int mx = maxHeap -> extractMax();
+        sum += mx;
+        maxHeap -> insert(mx - 1);
+    }
+    cout << sum;
+}
